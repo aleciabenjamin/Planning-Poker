02b5d47adb5351d21b94ef1d7d0679e9a330ab89
Alecia Benjamin <aleciabenjamin@gmail.com>
2020-04-28 15:05:33 +0200

    set up environment for unit testing

---
+++ backend/test/index.js
@@ -0,0 +1,18 @@
+const app = require("../app");
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+
+const { expect } = chai;
+chai.use(chaiHttp);
+describe("Server!", () => {
+  it("welcomes user to the api", (done) => {
+    chai
+      .request(app)
+      .get("/")
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.title).to.equals("Express");
+        done();
+      });
+  });
+});
