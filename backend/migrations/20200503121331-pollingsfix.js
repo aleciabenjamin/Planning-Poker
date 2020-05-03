c57d454ba2586bb7b2237430a5a8fd9f42576e47
Alecia Benjamin <aleciabenjamin@gmail.com>
2020-05-03 15:14:07 +0200

    update polling migration as it was not pluralized

--- backend/migrations/20200503121331-pollingsfix.js
+++ backend/migrations/20200503121331-pollingsfix.js
@@ -1,7 +1,7 @@
 "use strict";
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable("Polling", {
+    return queryInterface.createTable("Pollings", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -34,6 +34,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable("Polling");
+    return queryInterface.dropTable("Pollings");
   },
 };
