87e87e471c749ed5c9a63dbb9eb793f9b7107d0d
Alecia Benjamin <aleciabenjamin@gmail.com>
2020-05-14 15:02:59 +0200

    rename client to frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Provider } from "react-redux";
-import store from "store/index";
+import store from "store";
 import { createBrowserHistory } from "history";
 import { Router, Switch, Route } from "react-router-dom";
 import DefaultLayout from "layouts/DefaultLayout";
